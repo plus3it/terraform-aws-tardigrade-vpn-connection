0bb52b0584d005e5e2a151d772ada28526dce789
Cody Massey <cmassey@Administrators-MacBook-Pro.local>
2020-10-27 08:33:40 -0400

    removed any references to the create_...  in main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ locals {
 }
 
 resource "aws_vpn_gateway" "this" {
-  count = var.create_vpn_connection ? 1 : 0
 
   vpc_id          = var.vpc_id
   amazon_side_asn = var.amazon_side_asn
@@ -18,7 +17,6 @@ resource "aws_vpn_gateway" "this" {
 }
 
 resource "aws_customer_gateway" "this" {
-  count = var.create_vpn_connection ? length(var.cgw_ip_addresses) : 0
 
   bgp_asn    = var.cgw_bgp_asn
   ip_address = var.cgw_ip_addresses[count.index]
@@ -32,7 +30,6 @@ resource "aws_customer_gateway" "this" {
 }
 
 resource "aws_vpn_connection" "this" {
-  count = var.create_vpn_connection ? length(var.cgw_ip_addresses) : 0
 
   vpn_gateway_id      = aws_vpn_gateway.this[0].id
   customer_gateway_id = aws_customer_gateway.this[count.index].id
@@ -47,14 +44,12 @@ resource "aws_vpn_connection" "this" {
 }
 
 resource "aws_vpn_connection_route" "this" {
-  count = var.create_vpn_connection ? length(var.destination_cidr_blocks) : 0
 
   destination_cidr_block = var.destination_cidr_blocks[count.index]
   vpn_connection_id      = aws_vpn_connection.this[0].id
 }
 
 resource "aws_vpn_gateway_route_propagation" "this" {
-  count = var.create_vpn_connection ? var.propagating_route_table_count : 0
 
   vpn_gateway_id = aws_vpn_gateway.this[0].id
   route_table_id = var.propagating_route_table_ids[count.index]
